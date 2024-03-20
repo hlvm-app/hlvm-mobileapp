40c42f1f2be1d9e6608b6fdcf68419d49b9a3b28
TurtleOld <dev@pavlovteam.ru>
2024-03-20 07:14:04 +0300

    refactoring

--- lib/auth/auth.dart
+++ lib/auth/auth.dart
@@ -1,45 +1,62 @@
-class LoginForm extends StatefulWidget { 
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:hlvm_mobileapp/main.dart';
+import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
+import 'package:hlvm_mobileapp/state/state.dart';
+
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
   @override 
-  _LoginFormState createState() => _LoginFormState(); 
+  State<StatefulWidget> createState() => _LoginFormState();
 } 
  
 class _LoginFormState extends State<LoginForm> { 
   final _usernameController = TextEditingController(); 
   final _passwordController = TextEditingController(); 
  
-  Future<void> _login() async { 
-    final Uri url = Uri.parse('https://hlvm.ru/users/login/'); 
-    final response = await http.post(url, body: { 
+  Future<void> _login() async {
+    final response = await http.post(
+        Uri.parse('https://hlvm.ru/users/login/'), body: jsonEncode(<String, String>{
       'username': _usernameController.text, 
       'password': _passwordController.text, 
-    }); 
- 
-    if (response.statusCode == 200) { 
-      // Успешная авторизация 
+    }));
+    if (response.statusCode == 200) {
+      Provider.of<AuthProvider>(context, listen: false).setLoggedIn(true);
+      Navigator.pushReplacementNamed(context, '/MyHome');
+      print(response.body);
     } else { 
-      // Ошибка авторизации 
+      print('Неудача');
     } 
-  } 
- 
-  @override 
-  Widget build(BuildContext context) { 
-    return Column( 
-      mainAxisAlignment: MainAxisAlignment.center, 
-      children: <Widget>[ 
-        TextField( 
-          controller: _usernameController, 
-          decoration: InputDecoration(hintText: 'Введите имя пользователя'), 
-        ), 
-        TextField( 
-          controller: _passwordController, 
-          decoration: InputDecoration(hintText: 'Введите пароль'), 
-          obscureText: true, 
-        ), 
-        ElevatedButton( 
-          onPressed: _login, 
-          child: Text('Войти'), 
-        ), 
-      ], 
-    ); 
-  } 
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          TextField(
+            controller: _usernameController,
+            decoration: InputDecoration(hintText: 'Введите имя пользователя'),
+          ),
+          TextField(
+            controller: _passwordController,
+            decoration: InputDecoration(hintText: 'Введите пароль'),
+            obscureText: true,
+          ),
+          ElevatedButton(
+            onPressed: _login,
+            child: Text('Войти'),
+          ),
+        ],
+      ),
+    );
+  }
 }
---
+++ lib/state/state.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AuthProvider extends ChangeNotifier {
+  bool _isLoggedIn = false;
+
+  bool get isLoggedIn => _isLoggedIn;
+
+  void setLoggedIn(bool value) {
+    _isLoggedIn = value;
+    notifyListeners();
+  }
+
+  void clearLoggedIn() {
+    _isLoggedIn = false;
+    notifyListeners();
+  }
+}
