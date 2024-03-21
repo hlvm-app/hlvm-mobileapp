70cc5cc24ec253560394ee7739152a0bd9a25cb7
TurtleOld <dev@pavlovteam.ru>
2024-03-21 22:10:34 +0300

    refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,76 @@
 import 'package:flutter/material.dart';
-import 'package:hlvm_mobileapp/state/state.dart';
+import 'package:provider/provider.dart';
 import 'package:hlvm_mobileapp/auth/auth.dart';
-import 'package:hlvm_mobileapp/qr/qrview.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:qr_code_dart_scan/qr_code_dart_scan.dart';
 import 'package:hlvm_mobileapp/qr/live_decode.dart';
-import 'package:hlvm_mobileapp/qr/picture_decode.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SharedPreferences preferences = await SharedPreferences.getInstance();
   bool isLoggedIn = preferences.getBool('isLoggedIn') ?? false;
 
-  runApp(
-      MaterialApp(
-          home: isLoggedIn ? MyHome() : LoginForm(),
-          routes: {
-            '/MyHome': (context) => MyHome(),
-            LiveDecodePage.routeName: (context) => const LiveDecodePage(),
-            PictureDecode.routeName: (context) => const PictureDecode(),
-          },
-          debugShowCheckedModeBanner: false
-      ));
+  runApp(MaterialApp(
+      home: isLoggedIn ? MyHome() : LoginForm(),
+      routes: {
+        '/MyHome': (context) => MyHome(),
+        LiveDecodePage.routeName: (context) => const LiveDecodePage(),
+      },
+      debugShowCheckedModeBanner: false));
 }
 
-
-class MyHome extends StatelessWidget {
+class MyHome extends StatefulWidget {
   const MyHome({super.key});
+
+  @override
+  State<MyHome> createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  var selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Scan QR Code for HLVM')),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            ElevatedButton(
-                onPressed: () => LiveDecodePage.open(context),
-                child: const Text('Live Decode')
+      appBar: AppBar(
+          title: const Text(
+        'Hasta La Vista, Money!',
+        style: TextStyle(color: Colors.green),
+      )),
+      body: Row(
+        children: [
+          SafeArea(
+            child: NavigationRail(
+              extended: false,
+              destinations: [
+                NavigationRailDestination(
+                    icon: Icon(Icons.home), label: Text('Домашняя страница')),
+                NavigationRailDestination(
+                    icon: Icon(Icons.receipt), label: Text('Чеки')),
+              ],
+              selectedIndex: selectedIndex,
+              onDestinationSelected: (value) {
+                setState(() {
+                  selectedIndex = value;
+                });
+              },
             ),
-            ElevatedButton(
-                onPressed: () => PictureDecode.open(context),
-                child: const Text('Picture Decode')
-            )
-          ],
-        )
+          ),
+          Expanded(
+              child: Align(
+            alignment: Alignment.topRight,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                if (selectedIndex == 0 || selectedIndex == 1)
+                  ElevatedButton(
+                    onPressed: () => LiveDecodePage.open(context),
+                    child: const Icon(Icons.qr_code_scanner_outlined),
+                  ),
+              ],
+            ),
+          )),
+        ],
       ),
     );
   }
 }
-
--- lib/qr/live_decode.dart
+++ lib/qr/live_decode.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:qr_code_dart_scan/qr_code_dart_scan.dart';
+import 'package:vibration/vibration.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter/services.dart';
 
 ///
 /// Created by
@@ -14,13 +17,15 @@ import 'package:qr_code_dart_scan/qr_code_dart_scan.dart';
 /// on 28/06/22
 class LiveDecodePage extends StatefulWidget {
   static const routeName = '/live';
+
   static get route =>
       {routeName: (BuildContext context) => const LiveDecodePage()};
+
   static open(BuildContext context) {
     Navigator.of(context).pushNamed(routeName);
   }
 
-  const LiveDecodePage({Key? key}) : super(key: key);
+  const LiveDecodePage({super.key});
 
   @override
   LiveDecodePageState createState() => LiveDecodePageState();
@@ -28,10 +33,12 @@ class LiveDecodePage extends StatefulWidget {
 
 class LiveDecodePageState extends State<LiveDecodePage> {
   Result? currentResult;
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       body: QRCodeDartScanView(
         scanInvertedQRCode: true,
         onCapture: (Result result) {
@@ -54,11 +61,60 @@ class LiveDecodePageState extends State<LiveDecodePage> {
               children: [
                 Text('Text: ${currentResult?.text ?? 'Not found'}'),
                 Text('Format: ${currentResult?.barcodeFormat ?? 'Not found'}'),
+                Row(
+                  children: [
+                    ElevatedButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Icon(Icons.arrow_back)),
+                    SizedBox(width: 10.0,),
+                    ElevatedButton(
+                      onPressed: () {
+                        _copyToClipboard(currentResult!.text);
+                      },
+                      child: Text('Copy to Clipboard'),
+                    ),
+
+                  ],
+                ),
               ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  void _copyToClipboard(String? data) {
+    if (data != null) {
+      Clipboard.setData(ClipboardData(text: data));
+      _showSnackBar(context, 'Copied to clipboard');
+      _vibrate();
+      _launchURL('https://t.me/GetReceiptBot');
+    } else {
+      _showSnackBar(context, 'No data to copy');
+      _vibrate();
+    }
+  }
+
+  void _showSnackBar(BuildContext context, String message) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  void _vibrate() async {
+    bool? hasVibrator = await Vibration.hasVibrator();
+    if (hasVibrator != null && hasVibrator) {
+      Vibration.vibrate();
+    }
+  }
+
+  void _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
